c0614f33b1d175489d6aadf808c73ee0bff30f87
ArkBeer7 <breaker0717@gmail.com>
2017-02-05 15:57:43 +0900

    16th update
    test

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,18 @@
 #include"Header.hpp"
+#include"d3d11_1.hpp"
 Ark::FrameRate fps;
-Ark::D2D1_1 d1;
-
+Ark::D3D11_1 d11;
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	Ark::WinClass wc(hInstance);
 	wc.AddStyle(WS_THICKFRAME).SetTitle(_T("Title")).SetSize(300,200);
 	while (!wc.EndFlag()) {
 		auto s = std::chrono::steady_clock::now();
+		d11.CreateResource(wc.GethWnd());
 		wc.LockAspectRatio(10, 16);
-		d1.BeginDraw(wc.GethWnd());
-		d1.DrawClear(RGB(0, 0, 0));
+		d11.DrawClear();
 		fps.Count();
 		Ark::TstringStream tstr;
 		tstr << fps.perDuration<double>(s);
-		d1.DrawString(0, 0, tstr.str().c_str(), 0xffffff, 1.0f, 10);
-		d1.EndDraw();
 	}
 	return 0;
 }
