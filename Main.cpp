a97abc6b3e5b31a5bdaccdafc687f97904a9ba7b
ArkBeer <breaker0717@gmail.com>
2017-06-05 11:30:16 +0900

    wic
    add wic header.

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,29 @@
 #include"Header.hpp"
 #include"D3D11_1.hpp"
+#include"WIC.hpp"
+#include<array>
+#include<vector>
+
 Ark::FrameRate fps;
+Ark::Random rnd;
+Ark::Key key;
+
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
 	Ark::WinClass wc(hInstance);
 	wc.AddStyle(WS_THICKFRAME).SetTitle(_T("Title")).SetSize(300,200);
 	Ark::D3D11_1 d;
+	Ark::WIC w;
+	auto image=w.loadimage(_T("icon.png"));
 	while (!wc.EndFlag()) {
 		auto s = std::chrono::steady_clock::now();
-		wc.LockAspectRatio(10, 16);
+		wc.LockAspectRatio(9, 16);
 		d.BeginDraw(wc.GethWnd());
 		d.DrawClear();
-		d.Draw();
 		d.EndDraw();
 		fps.Count();
 		Ark::TstringStream tstr;
-		tstr << fps.perDuration<double>(s);
+		tstr << fps.perDuration<double>(s)<<"\n";
+		OutputDebugString(tstr.str().c_str());
 	}
 	return 0;
 }
